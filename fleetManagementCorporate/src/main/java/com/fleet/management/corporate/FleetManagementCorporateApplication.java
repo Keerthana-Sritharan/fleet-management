ca50cf9e3702612e5ca2a93584dc0dac124fea5e
unknown <sritharankeerthana1997@gmail.com>
2022-06-21 09:14:51 +0530

    code structure for corporate service

---
+++ fleetManagementCorporate/src/main/java/com/fleet/management/corporate/FleetManagementCorporateApplication.java
@@ -0,0 +1,13 @@
+package com.fleet.management.corporate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetManagementCorporateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetManagementCorporateApplication.class, args);
+	}
+
+}
---
+++ fleetManagementCorporate/src/test/java/com/fleet/management/corporate/FleetManagementCorporateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleet.management.corporate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetManagementCorporateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
