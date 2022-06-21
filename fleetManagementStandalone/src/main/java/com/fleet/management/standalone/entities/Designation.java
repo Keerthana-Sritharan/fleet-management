00a6036d16c54e46dafd9a1a2a411cdb5c253909
unknown <sritharankeerthana1997@gmail.com>
2022-06-21 10:17:18 +0530

    Designation entity

---
+++ fleetManagementStandalone/src/main/java/com/fleet/management/standalone/entities/Designation.java
@@ -0,0 +1,11 @@
+package com.fleet.management.standalone.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Designation {
+private Long id;
+private String Name;
+}
